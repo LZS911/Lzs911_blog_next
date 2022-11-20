e4d5341b91d96c065c939c1d8d308e02cee78e34
LZS911 <932177767@qq.com>
2022-11-20 21:13:13 +0800

    [feature]: init project

---
+++ interfaces/post.ts
@@ -0,0 +1,18 @@
+import type Author from './author';
+
+type PostType = {
+  slug: string;
+  title: string;
+  date: string;
+  coverImage: string;
+  author: Author;
+  excerpt: string;
+  ogImage: {
+    url: string;
+  };
+  content: string;
+  star: boolean;
+  category: 'blog' | 'project';
+};
+
+export default PostType;
